233bd843c2dc97d69024fc1eee2158bf901e7cea
gourteacher <ouaazs@algonquincollege.com>
2022-06-12 17:56:36 -0400

    Week6 - SQLite example using RecyclerView

--- app/src/main/java/com/college/exercises_s22/MainActivity.java
+++ app/src/main/java/com/college/exercises_s22/MainActivity.java
@@ -1,6 +1,7 @@
 package com.college.exercises_s22;
-
-
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,12 +10,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -23,13 +25,16 @@ import java.util.Locale;
 
 
 //This makes it a page in your application
-public class FirstActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
+    //Create an OpenHelper to store data:
+    private MyOpenHelper myOpener;
+    private SQLiteDatabase theDatabase;
     //use them anywhere in the class:
-    Button submit;
-    EditText edit;
-    RecyclerView rView;
-    MyAdapter theAdapter;   //<<cannot be anonymous<<
-    ArrayList<Message> messages = new ArrayList<>();
+    private Button submit;
+    private EditText edit;
+    private RecyclerView rView;
+    private MyAdapter theAdapter;
+    private ArrayList<Message> messages = new ArrayList<>();
 
     //need onCreate:
     @Override
@@ -37,7 +42,34 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(p);
 
         //load XML:
-        setContentView(R.layout.activity_first);
+        setContentView(R.layout.activity_main);
+
+        //initialize it in onCreate
+        myOpener = new MyOpenHelper( this );
+        //open the database:
+        theDatabase = myOpener.getWritableDatabase();
+
+        //load from the database:
+        Cursor results = theDatabase.rawQuery( "Select * from " + MyOpenHelper.TABLE_NAME + ";", null );//no arguments to the query
+
+        //Convert column names to indices:
+        int idIndex = results.getColumnIndex( MyOpenHelper.COL_ID );
+        int  messageIndex = results.getColumnIndex( MyOpenHelper.COL_MESSAGE);
+        int sOrRIndex = results.getColumnIndex( MyOpenHelper.COL_SEND_RECEIVE);
+        int timeIndex = results.getColumnIndex( MyOpenHelper.COL_TIME_SENT );
+
+        //cursor is pointing to row -1
+        while( results.moveToNext() ) //returns false if no more data
+        { //pointing to row 2
+            int id = results.getInt(idIndex);
+            String message = results.getString( messageIndex );
+            String time = results.getString( timeIndex);
+
+            //add to arrayList:
+            messages.add( new Message( message, time, id ));
+        }
+
+        results.close();
 
         submit = findViewById(R.id.submitButton);
         edit = findViewById(R.id.editText);
@@ -46,27 +78,45 @@ public class FirstActivity extends AppCompatActivity {
         theAdapter = new MyAdapter();
         rView.setAdapter( theAdapter ) ;
         rView.setLayoutManager(new LinearLayoutManager(this));
-        //rView.setLayoutManager(new GridLayoutManager (this, 2) );
-
 
         submit.setOnClickListener( click ->{
             String whatIsTyped = edit.getText().toString();
-            Date timeNow = new Date(); //when was this code run
 
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
+            if (!whatIsTyped.isEmpty()) {
 
-            String currentDateandTime = sdf.format( timeNow ); //convert date to String
+                Date timeNow = new Date(); //when was this code run
 
-            //adding a new message to our history if not empty
-            if ( !whatIsTyped.isEmpty()) {
-                messages.add(new Message(whatIsTyped, currentDateandTime));
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
+                String currentDateandTime = sdf.format(timeNow); //convert date to String
+
+                //insert into database:
+                ContentValues newRow = new ContentValues();// like intent or Bundle
+
+                //Message column:
+                newRow.put(MyOpenHelper.COL_MESSAGE, whatIsTyped);
+
+                //Send or receive column:
+                newRow.put(MyOpenHelper.COL_SEND_RECEIVE, 1);
+
+                //TimeSent column:
+                newRow.put(MyOpenHelper.COL_TIME_SENT, currentDateandTime);
+
+                //now that columns are full, you insert:
+
+                long id = theDatabase.insert(MyOpenHelper.TABLE_NAME, null, newRow); //returns the id
+
+                Message cm = new Message(whatIsTyped, currentDateandTime, id);
+
+                //adding a new message to our history:
+                messages.add(cm); //what is the database id?
 
                 edit.setText("");//clear the text
 
                 //notify that new data was added at a row:
                 theAdapter.notifyItemInserted(messages.size() - 1); //at the end of ArrayList,
-
+                // call onCreateViewHolder, onBindViewHolder()
             }
+
         });
 
     }
@@ -79,13 +129,13 @@ public class FirstActivity extends AppCompatActivity {
         //returning it to the RecyclerView.
         @NonNull
         @Override
-        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
             //Load a new row from the layout file:
             LayoutInflater li = getLayoutInflater();
 
             //import layout for a row:
-            View thisRow = li.inflate(R.layout.row_layout, parent, false);
+            View thisRow = li.inflate(R.layout.sent_message, parent, false);
 
             return new MyViewHolder( thisRow );
         }
@@ -133,7 +183,7 @@ public class FirstActivity extends AppCompatActivity {
                 int position = getAdapterPosition();//which row was clicked.
                 Message whatWasClicked = messages.get(position);
 
-                AlertDialog.Builder builder = new AlertDialog.Builder( FirstActivity.this );
+                AlertDialog.Builder builder = new AlertDialog.Builder( MainActivity.this );
 
                 builder.setTitle("Question:")
                         .setMessage("Do you want to delete this:" + whatWasClicked.getMessageTyped())
@@ -142,6 +192,19 @@ public class FirstActivity extends AppCompatActivity {
                             //actually delete something:
                             messages.remove(position);
                             theAdapter.notifyItemRemoved(position);
+                            Snackbar.make(submit, "You removed item # " + position, Snackbar.LENGTH_LONG)
+                                    .setAction("Undo", (click4)-> {
+                                        messages.add(position, whatWasClicked);
+                                        theAdapter.notifyItemInserted(position);
+                                        //reinsert into the database
+                                        theDatabase.execSQL( String.format( Locale.CANADA, " Insert into %s values (\"%d\", \"%s\", \"%d\", \"%s\" );",
+                                                MyOpenHelper.TABLE_NAME      , whatWasClicked.getId()  , whatWasClicked.getMessageTyped() , 1  , whatWasClicked.getTimeSent()));
+
+                                    })
+                                    .show();
+                            //delete from database:, returns number of rows deleted
+                            theDatabase.delete(MyOpenHelper.TABLE_NAME,
+                                    MyOpenHelper.COL_ID +" = ?", new String[] { Long.toString( whatWasClicked.getId() )  });
                         }).create().show();
             });
         }
@@ -150,10 +213,12 @@ public class FirstActivity extends AppCompatActivity {
     public class Message{
         String messageTyped;
         String timeSent;
+        long id;
 
-        public Message(String messageTyped, String timeSent) {
+        public Message(String messageTyped, String timeSent, long _id) {
             this.messageTyped = messageTyped;
             this.timeSent = timeSent;
+            this.id = _id;
         }
 
         public String getMessageTyped() {
@@ -162,6 +227,13 @@ public class FirstActivity extends AppCompatActivity {
 
         public String getTimeSent() {
             return timeSent;
+        }
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
         }
     }
 
